d047bf7d25a8759615e4c9368f6021cbf9356aa3
salemaziel <mymainemail0501@gmail.com>
2020-12-23 19:38:42 -0800

    added directorii badge

--- src/components/PageComponents/Home/CustomerLovesSection.js
+++ src/components/PageComponents/Home/CustomerLovesSection.js
@@ -11,7 +11,7 @@ import {
   BadgeHAtoprated,
 } from "../../../images/index"
 
-import Fade from 'react-reveal/Fade'
+import Fade from "react-reveal/Fade"
 
 import customerlovesStyle from "./customerloves.module.css"
 
@@ -22,30 +22,35 @@ class CustomerLovesSection extends React.Component {
         <Container>
           <Row>
             <Col>
-              <div className={customerlovesStyle.Intro} >
+              <div className={customerlovesStyle.Intro}>
                 <Fade top>
-                <h1 className={customerlovesStyle.IntroTitle} >
-                  Zenith Roofing Services proudly serves the following Southern California Counties: <br /> San Diego, Temecula, Orange, Riverside and Los Angeles.{" "}
-                </h1>
+                  <h1 className={customerlovesStyle.IntroTitle}>
+                    Zenith Roofing Services proudly serves the following
+                    Southern California Counties: <br /> San Diego, Temecula,
+                    Orange, Riverside and Los Angeles.{" "}
+                  </h1>
                 </Fade>
                 <Fade top>
-                <p className={customerlovesStyle.IntroText}>
-                  With years of experience serving the greater Southern California
-                  area, Zenith Roofing Services is
-                  the solution for all your roofing needs. </p>
+                  <p className={customerlovesStyle.IntroText}>
+                    With years of experience serving the greater Southern
+                    California area, Zenith Roofing Services is the solution for
+                    all your roofing needs.{" "}
+                  </p>
 
                   <p className={customerlovesStyle.IntroText}>
-                  We take great pride in our work and prioritize extreme
-                  professionalism, timeliness, and superior customer service on
-                  every job. We strive to develop lasting relationships with all of our
-                  customers by getting the job done so well, the next time
-                  roofing service is needed, there is is no question who to call.
-                </p>
+                    We take great pride in our work and prioritize extreme
+                    professionalism, timeliness, and superior customer service
+                    on every job. We strive to develop lasting relationships
+                    with all of our customers by getting the job done so well,
+                    the next time roofing service is needed, there is is no
+                    question who to call.
+                  </p>
 
-                <p className={customerlovesStyle.IntroText}>
-                  Contact us today so we can discuss your needs and make a plan
-                  that fits your budget.<br /> <strong> Satisfaction guaranteed!</strong>
-                </p>
+                  <p className={customerlovesStyle.IntroText}>
+                    Contact us today so we can discuss your needs and make a
+                    plan that fits your budget.
+                    <br /> <strong> Satisfaction guaranteed!</strong>
+                  </p>
                 </Fade>
               </div>
             </Col>
@@ -57,29 +62,47 @@ class CustomerLovesSection extends React.Component {
             <Col>
               <div className={customerlovesStyle.WhyloveUs}>
                 <Fade top>
-                <h1 className={customerlovesStyle.WhyTitle}>
-                  {" "}
-                  Why Choose Zenith Roofing?
-                </h1>
+                  <h1 className={customerlovesStyle.WhyTitle}>
+                    {" "}
+                    Why Choose Zenith Roofing?
+                  </h1>
 
-                <br />
+                  <br />
 
-                <h2 className={customerlovesStyle.WhySubtitle}>
-                  Our Customers Love Us!
-                </h2>
+                  <h2 className={customerlovesStyle.WhySubtitle}>
+                    Our Customers Love Us!
+                  </h2>
 
-                <p>
-                  Check out our 5 star reviews from happy customers! Call us
-                  today so we can discuss your needs and make a plan that fits
-                  your budget. Satisfaction guaranteed!
-                </p>
+                  <p>
+                    Check out our 5 star reviews from happy customers! Call us
+                    today so we can discuss your needs and make a plan that fits
+                    your budget. Satisfaction guaranteed!
+                  </p>
                 </Fade>
               </div>
+              <Row>
+              <Col>
+
+              <a
+                style={{ display: "flex" }}
+                href="https://roofinginsights.com/directory/"
+                className={customerlovesStyle.Badges}
+                              >
+                <img
+                  /*style={{ maxWidth: 200, height: "auto" }}*/
+                  alt="roofers near me, roofing near me, roofing contractor near me, roofing directory, roofers directory, trusted roofers"
+                  src="https://roofinginsights.com/wp-content/uploads/2020/05/certified-contractor-roofing-directory-roofers-near-me.png?e2a1a8&e2a1a8"
+                  className={customerlovesStyle.Badges}
+                />
+              </a>
+              </Col>
+            </Row>
             </Col>
-            <Col className="Nomobile"/>
+            <Col className="Nomobile" />
           </Row>
           <Row>
             <Col>
+
               <div className={customerlovesStyle.Badgesbox}>
                 <img
                   src={BadgeHAtoprated}
@@ -97,6 +120,7 @@ class CustomerLovesSection extends React.Component {
                   className={customerlovesStyle.Badges}
                 />
               </div>
+
             </Col>
 
             <Col xs={5} className={customerlovesStyle.ColGuy}>
